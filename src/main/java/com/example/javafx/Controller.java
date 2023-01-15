b957973bc70c3533490363b81b52e8a7a73fa498
gedance18 <gedance18yt@gmail.com>
2023-01-15 11:43:38 +0100

    Initial commit

---
+++ src/main/java/com/example/javafx/Controller.java
@@ -0,0 +1,30 @@
+package com.example.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML
+    Button b1;
+    @FXML
+    Button b2;
+    @FXML
+    Button b3;
+    @FXML
+    Button b4;
+    @FXML
+    Button b5;
+    @FXML
+    Button b6;
+    @FXML
+    Button b7;
+    @FXML
+    Button b8;
+    @FXML
+    Button b9;
+
+
+
+
+}
